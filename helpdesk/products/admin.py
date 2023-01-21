93096ef583e341513962a6b5f7dae2fbfa4002bc
Sergey Gumenyuk <audi-1988@inbox.ru>
2023-01-21 16:11:25 +0700

    add Product model

--- helpdesk/products/admin.py
+++ helpdesk/products/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from products.models import Product
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price', 'is_available', 'created', 'updated']
+    list_filter = ['title', 'created', 'updated', 'price']
+    list_editable = ['is_available']
+    prepopulated_fields = {'slug': ('title',)}
--- helpdesk/products/models.py
+++ helpdesk/products/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    is_available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('product_detail', args=[self.slug])
