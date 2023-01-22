915d5984f1bfaf3eb020cab0c16e58312f18ef83
Sergey Gumenyuk <audi-1988@inbox.ru>
2023-01-22 18:01:34 +0700

    add registration new user (user_register view and UserRegistrationForm)

--- helpdesk/customers/urls.py
+++ helpdesk/customers/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('user/login/', views.user_login, name='login'),
     path('user/logout/', views.user_logout, name='logout'),
+    path('user/register/', views.user_register, name='registration'),
 
 ]
--- helpdesk/customers/views.py
+++ helpdesk/customers/views.py
@@ -2,11 +2,11 @@ from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
-from customers.forms import UserLoginForm
+from customers.forms import UserLoginForm, UserRegistrationForm
 
 
 def index(request):
-    return HttpResponse('Lets start')
+    return render(request, 'customers/home.html')
 
 
 def user_login(request):
@@ -19,6 +19,7 @@ def user_login(request):
                                 password=cd['password'])
             if user is not None:
                 login(request, user)
+                return redirect('customers:home')
     else:
         form = UserLoginForm()
 
@@ -28,3 +29,18 @@ def user_login(request):
 def user_logout(request):
     logout(request)
     return redirect('customers:login')
+
+
+def user_register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save(commit=False)
+            new_user.set_password(form.cleaned_data['password'])
+            new_user.save()
+            login(request, new_user)
+            return redirect('customers:home')
+    else:
+        form = UserRegistrationForm()
+
+    return render(request, 'customers/register.html', {'form': form})
